2ad09a44dd9012153ee33f8c44990fc3a772933e
k1n6o8 <47073844+k1n6o8@users.noreply.github.com>
2020-07-20 14:31:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const functions = {
   gradeClassifier: (grade) => {
-    // TODO: Write code here
+    if(grade <=100 && grade >= 95.00)
+        return "O - Outstanding";
+    else if (grade < 95.00  && grade >= 90.00)
+        return "V - Very Good";
+    else if (grade < 90.00  && grade >= 85.00)
+        return "G - Good";
+    else if (grade < 85.00  && grade >= 80.00)
+        return "S - Satisfactory";
+    else if (grade < 80.00  && grade >= 75.00)
+        return "N - Needs Improvement";
+    else if (grade < 75.00 && grade >= 0.00)
+        return "D - Did not Meet Expectations";
+    else
+        return "Invalid Input"
   }
 }
 
